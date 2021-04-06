68eae1c30835e186d3ec9e170230e04a70cefe25
Latifur Rahman <latifurrahman350@gmail.com>
2021-04-06 23:17:53 +0600

    project done

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './Main.css';
 import Player from '../../components/Player/Player';
 import PlayerData from '../../components/data/data.json';
+import Member from '../Member/Member';
 
 const Main = () => {
     const [players, setPlayers] = useState([]);
+    const [member, setMember] = useState([]);
+
     const handleAddPlayer = (player) =>{
-        console.log('added', player)
+        console.log('added', player);
+        const newMember = [...member, player];
+        setMember(newMember);
     }
     useEffect(()=>{
         setPlayers(PlayerData);
@@ -22,6 +27,7 @@ const Main = () => {
 
             <div className="main-right">
                 <h1><u>Khulna Tigers Club</u></h1>
+                <Member member={member}></Member>
             </div>
             
         </div>
---
+++ src/components/Member/Member.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Member.css';
+
+const Member = (props) => {
+    const members = props.member;
+    const total = members.reduce((total, element) => total + element.salary , 0)
+    console.log(members);
+    return (
+        <div className="members-detail">
+            <h2>Added Player: {members.length}</h2>
+            <div className="player-name">
+                <h3>Players Name:</h3>
+                {
+                    members.map(member => <h6>{member.name}</h6>)
+                }
+            </div>
+            
+                <h3 className="budget">Total Budget: {total}</h3>
+        </div>
+    );
+};
+
+export default Member;
